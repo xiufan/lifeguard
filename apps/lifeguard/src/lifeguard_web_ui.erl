12662ae32c1897745722a960d85e97d1f856511e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-04-30 15:05:25 -0700

    Static page routes, redirect index to a static page

--- apps/lifeguard/src/lifeguard_web_ui.erl
+++ apps/lifeguard/src/lifeguard_web_ui.erl
@@ -1,5 +1,6 @@
 -module(lifeguard_web_ui).
--export([init/1, to_html/2]).
+-export([init/1,
+         to_html/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
@@ -7,5 +8,8 @@ init(_) ->
     {ok, no_context}.
 
 to_html(ReqData, Context) ->
-    {ok, Content} = index_dtl:render([]),
-    {Content, ReqData, Context}.
+    % Redirect to "/pages/index.html" so that we can just use the static
+    % folders for our stuff.
+    Response  = wrq:set_resp_header("location", "/pages/index.html", ReqData),
+    Response1 = wrq:do_redirect(true, Response),
+    {{halt, 302}, Response1, Context}.
