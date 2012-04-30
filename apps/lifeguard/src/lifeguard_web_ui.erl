277ea3c8e4634d42c0a500b42256c1920f364f2e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-04-30 15:29:38 -0700

    Render templates

--- apps/lifeguard/src/lifeguard_web_ui.erl
+++ apps/lifeguard/src/lifeguard_web_ui.erl
@@ -1,15 +1,38 @@
 -module(lifeguard_web_ui).
 -export([init/1,
+         resource_exists/2,
          to_html/2]).
+
+-record(state, {
+        content % The content to send to the browser
+        }).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
 init(_) ->
-    {ok, no_context}.
+    {ok, #state{}}.
+
+resource_exists(ReqData, Context) ->
+    % Replace the "/" with "_" in paths, so requests to pages such as
+    % "filters/new" turns into "filters_new." Finally, "_dtl" is
+    % appended onto the end of the module name since that is how it is
+    % compiled.
+    Path       = case wrq:disp_path(ReqData) of
+        ""    -> "index";
+        Other -> Other
+    end,
+    BinPath    = list_to_binary(Path),
+    ModuleName = binary:replace(BinPath, <<"/">>, <<"_">>),
+    Module     = binary_to_atom(<<ModuleName/binary, "_dtl">>, utf8),
+
+    % Attempt to call the module. If this fails, then the resource
+    % doesn't actually exist.
+    try apply(Module, render, [[]]) of
+        {ok, Content} -> {true, ReqData, Context#state{content=Content}}
+    catch
+        error: undef -> {false, ReqData, Context}
+    end.
 
 to_html(ReqData, Context) ->
-    % Redirect to "/pages/index.html" so that we can just use the static
-    % folders for our stuff.
-    Response  = wrq:set_resp_header("location", "/pages/index.html", ReqData),
-    Response1 = wrq:do_redirect(true, Response),
-    {{halt, 302}, Response1, Context}.
+    % We just shuttle out the content since we already rendered
+    {Context#state.content, ReqData, Context}.
